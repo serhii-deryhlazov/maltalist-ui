f074a52c7fae05cbc1fcaaa47f7a342097498719
deryhlazov <Serhii.Deryhlazov@quest.com>
2025-04-21 11:34:28 +0200

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 import { UserProfileService } from '/assets/js/services/userProfileService.js';
 import { CacheService } from '/assets/js/services/cacheService.js';
 import { ListingService } from '/assets/js/services/listingService.js';
-import { UserProfileService } from '/assets/js/services/userProfileService.js';
 
 $(document).ready(function() {
     function loadContent(page) {
